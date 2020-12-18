3713343d12802feb4689a3109f09600d8886fc4b
Kevin Purnelle <kevin.purnelle@gmail.com>
2020-12-18 15:10:24 +0100

    refactor(staking): ethereumUtils

--- packages/staking-dashboard/src/lib/ethereumUtils.ts
+++ packages/staking-dashboard/src/lib/ethereumUtils.ts
@@ -1,18 +1,17 @@
 type networks = 'mainnet' | 'ropsten' | 'rinkeby' | 'kovan'
 
+enum networkIds {
+  mainnet = 1,
+  ropsten = 3,
+  rinkeby = 4,
+  kovan = 42
+}
+
 function getNetworkIdByString(networkName: networks | undefined): 1 | 3 | 4 | 42 | 0 {
-  switch (networkName) {
-    case 'mainnet':
-      return 1
-    case 'ropsten':
-      return 3
-    case 'rinkeby':
-      return 4
-    case 'kovan':
-      return 42
-    default:
-      return 0
+  if (!networkName || !networkIds[networkName]) {
+    return 0
   }
+  return networkIds[networkName]
 }
 
 export default {
